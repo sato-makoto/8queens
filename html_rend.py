3068dcf8584867e4744a1957ab6fba8e34f2e934
Sato Makoto <makoto@kuzuore.com>
2015-07-10 22:16:58 +0900

    cannot run, yet

--- html_rend.py
+++ html_rend.py
@@ -1,7 +1,14 @@
 include eightqueen_func as eq
 
 def html_header():
- print '' 
+ header_file = 'html_header'
+ h = open(header_file)
+ return h
+
+def html_footer():
+ footer_file = 'html_footer'
+ f = open(footer_file)
+ return f
 
 def h1_out(eq.width):
   print '<h1>{} Queens</h1>'.format(eq.width)
@@ -19,9 +26,10 @@ def table_first_line(eq.width):
     print '<td class="none">{}</td>'.format(x)
   print '</tr>'
 
-def a_line_out(queens_list, eq.width):
+def a_lines_out(queens_list, eq.width):
   wgtaple = classtaple[2:]
   for x in range(eq.width):
+    print '<tr>\n <td class="left">{}</td>'.format(x)
     for y in queens_list:
       if x = y[1]:
 
