8b95598e8754aa5f09c24084c039fcbab6253afa
Sato Makoto <makoto@kuzuore.com>
2015-07-11 09:08:20 +0900

    last one mile

--- e_html.py
+++ e_html.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 import eightqueen_func as eq
+import html_rend as html
 
 x, y, pairs, myset = eq.allclear()
 mylist = eq.get_queens_pattern()
 
-print mylist
+head = html.html_header()
+for x in head:
+  print x[:-1]
+
+print html.h1_out(eq.width)
+print html.first_line()
+html.table_first_line(eq.width)
+
+html.lines_out(mylist, eq.width)
+
+foot = html.html_footer()
+for x in foot:
+  print x[:-1]
+
--- html_rend.py
+++ html_rend.py
@@ -1,4 +1,12 @@
-include eightqueen_func as eq
+import eightqueen_func as eq
+
+classtaple = ('none', 'left', 'white', 'gray')
+
+def cross_x(qlist, width):
+  clist = [None for y in range(width)]
+  for x in qlist:
+    clist[x[0]] =  x
+  return clist  
 
 def html_header():
  header_file = 'html_header'
@@ -10,30 +18,29 @@ def html_footer():
  f = open(footer_file)
  return f
 
-def h1_out(eq.width):
-  print '<h1>{} Queens</h1>'.format(eq.width)
+def h1_out(width):
+ return '<h1>{} Queens</h1>'.format(eq.width)
 
-def first_line(eq.width):
-  print '<table>'
 
-def css_class_name():
- classtaple = ('none', 'left', 'white', 'gray')
- return classtaple
+def first_line():
+  return '<table>'
 
-def table_first_line(eq.width):
+def table_first_line(width):
   print '<tr>\n  <td class="none"></td>'
-  for x in range(eq.width):
-    print '<td class="none">{}</td>'.format(x)
+  for x in range(width):
+    print '  <td class="none">{}</td>'.format(x)
   print '</tr>'
 
-def a_lines_out(queens_list, eq.width):
-  wgtaple = classtaple[2:]
-  for x in range(eq.width):
+def lines_out(queens_list,width):
+  wglist = classtaple[2:]
+  new_queen_list = cross_x(queens_list, width)
+  for x in range(width):
+    begin_color = x%2
     print '<tr>\n <td class="left">{}</td>'.format(x)
     for y in queens_list:
-      if x = y[1]:
-
-def html_footer():
- print '' 
+      if y[1] = 
 
 
+      print '  <td class="{}"></td>'.format(wglist[begin_color])
+      begin_color = (begin_color + 1) % 2
+    print '</tr>'
