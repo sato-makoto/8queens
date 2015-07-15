578aab3a7f807e3a4bf4e86f77438ce06634c785
Sato Makoto <makoto@kuzuore.com>
2015-07-15 11:58:11 +0900

    slow and print queens

--- watch_try_and_error.py
+++ watch_try_and_error.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python
 import random
-import time
+from time import sleep
 import eightqueen_func as eq
 
-def get_deb_queens_pattern():
-  print "{} Queens Start!".format(eq.width)
-  i = 0
-  x, y, pairs, myset = eq.allclear()
+def main(width):
+  error = 0
+  x, y, pairs, mylist = eq.allclear()
   while y < eq.width:
-    time.sleep(0.5)
-    if y == eq.width - 1 and eq.rest_line_length(y, myset) == 1:
-      temp = myset.pop()
-      x, y = int(temp[0]), int(temp[2])
+#    sleep(0.5) 
+    next_list = eq.ylist(y, mylist)
+    if y == eq.last and len(next_list) == 1:
+      pairs.append(next_list[0])
+      break 
+    if len(next_list) > 0:
+      x = next_list[eq.randselect(len(next_list))][0]
+      mylist = eq.remove_list(x, y, mylist)
       pairs.append([x, y])
-      print "After {} times Fault, Now Success!".format(i)
-      break
+      y+=1
+      print pairs
     else:
-      if eq.rest_line_length(y, myset):
-        eq.mybatch(x, y, myset)
-        pairs.append([x, y])
-        print pairs
-        y+=1
-        yline = eq.ylines(y, myset)
-        ylength = len(yline)
-        if ylength:
-          x = int(yline[eq.randselect(ylength)][0])
-      else:
-        i+=1
-        print i, "Times Fault!" 
-        x, y, pairs, myset = eq.allclear()
-  return  pairs
+      error += 1
+      print '{} times error!'.format(error) 
+      x, y, pairs, mylist = eq.allclear()
+  return pairs
 
 
-x, y, pairs, myset = eq.allclear()
 
-pairs =  get_deb_queens_pattern()
+x, y, pairs, mylist = eq.allclear()
+
+pairs =  main(eq.width)
 print pairs
-
